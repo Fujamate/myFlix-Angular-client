4ee1798aefa351b1b8bab3db9e3be1f89a54d0d7
Kevin Hölscher <kevin-hoelscher@gmx.de>
2024-04-14 02:02:28 +0200

    moved the dialogs to the welcome page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,12 @@
-// src/app/app.component.ts
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { MatDialog } from '@angular/material/dialog';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // standalone: true,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
 
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: '500px'
-    });
-  }
-
-  constructor(public dialog: MatDialog) { }
-// This is the function that will open the dialog when the signup button is clicked  
-openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-// Assigning the dialog a width
-    width: '280px'
-    });
-  }
-
-   // This is the function that will open the dialog when the login button is clicked  
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      // Assigning the dialog a width
-      width: '280px'
-    });
-  }
 }
