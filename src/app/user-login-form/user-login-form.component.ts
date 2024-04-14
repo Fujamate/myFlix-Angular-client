5c09b8ad517d57ad3ad56992f9a0384c2957042e
Kevin Hölscher <kevin-hoelscher@gmx.de>
2024-04-14 02:17:52 +0200

    typos

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,41 +1,58 @@
+// src/app/user-registration-form/user-registration-form.component.ts
 import { Component, OnInit, Input } from '@angular/core';
+
+// You'll use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
+
+// This import brings in the API calls we created in 6.2
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+// This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { UserRegistrationService } from '../fetch-api-data.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
-  styleUrls: ['./user-login-form.component.scss']
+  styleUrl: './user-login-form.component.scss',
 })
 export class UserLoginFormComponent implements OnInit {
-
   @Input() userData = { Username: '', Password: '' };
 
   constructor(
-    public fetchApiData: UserRegistrationService,
+    public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
     public snackBar: MatSnackBar,
-    public router: Router ) {}
+    public router: Router
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  // This is the function responsible for sending the form inputs to the backend
+  /**
+     * This is the function responsible for sending the form inputs to the backend
+     * @returns user logged in
+     * @returns user navigated to movies view
+     * @returns user token and user details saved to local storage
+     * @returns user notified of success
+     * @returns user notified of error
+     */
+
   loginUser(): void {
     this.fetchApiData.userLogin(this.userData).subscribe((result) => {
-      console.log(result)
-      // Logic for a successful user registration goes here! (To be implemented)
+      // Logic for a successful user login goes here! (To be implemented)
+      console.log(result);
+      localStorage.setItem('user', JSON.stringify(result.user));
+      localStorage.setItem('token', result.token);
       this.dialogRef.close(); // This will close the modal on success!
-      this.snackBar.open('user logged in successfully!', 'OK', {
+      console.log(result);
+      this.snackBar.open('Logged in', 'OK', {
         duration: 2000
       });
+      // Successfully login done
+      
       this.router.navigate(['movies']);
-      localStorage.setItem('user', JSON.stringify(result.user));
-      localStorage.setItem('token', JSON.stringify(result.token));
-    }, (response) => {
-      this.snackBar.open(response, 'OK', {
+    }, (result) => {
+      this.snackBar.open('Login failed', 'OK', {
         duration: 2000
       });
     });
