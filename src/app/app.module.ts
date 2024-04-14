393b8c90f3fc0299bd95797343ba19acd34213d2
Kevin Hölscher <kevin-hoelscher@gmx.de>
2024-04-14 02:18:26 +0200

    imported moviecardcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,7 @@
-// app.module.ts
-
-import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-// Materials Imports
+import { RouterModule, Routes } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -17,11 +9,18 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { DialogModule } from '@angular/cdk/dialog';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AppComponent } from './app.component';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-import { MatIconModule } from '@angular/material/icon';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
@@ -38,24 +37,29 @@ const appRoutes: Routes = [
     WelcomePageComponent,
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
+    
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    DialogModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
     MatInputModule,
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatDialogModule,
     MatSnackBarModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes),
     MatIconModule,
+    MatMenuModule,
+    FormsModule,
+    MatListModule,
+    MatToolbarModule
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
-
-
