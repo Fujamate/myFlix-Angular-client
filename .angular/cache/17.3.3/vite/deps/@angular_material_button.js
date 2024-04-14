ec1b76f477bfe16f84a5057f93410ae408e0e014
Kevin Hölscher <kevin-hoelscher@gmx.de>
2024-04-14 13:26:22 +0200

    fixed

---
+++ .angular/cache/17.3.3/vite/deps/@angular_material_button.js
@@ -0,0 +1,36 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-RFCMILDP.js";
+import "./chunk-VPUDWOGO.js";
+import "./chunk-F2O2IT2Y.js";
+import "./chunk-DGV72MND.js";
+import "./chunk-4RVK6YC4.js";
+import "./chunk-UKEHM6V6.js";
+import "./chunk-V2DXGMIT.js";
+import "./chunk-ZDOIMVJD.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
